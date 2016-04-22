dfdf4f2c04502e87180f70e3822be49ee3405d15
Tomaco <tomgdieguez@gmail.com>
2016-04-22 19:22:39 -0300

    Correct typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// Bower packages
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
